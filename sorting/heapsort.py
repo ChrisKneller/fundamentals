2eb3d682e0de271d8bb9eec8b01a3cedb4423b6a
Chris <christian.kneller@gmail.com>
2020-05-04 11:58:12 +0100

    Added maxheap class for heapsort implementation

--- sorting/heapsort.py
+++ sorting/heapsort.py
@@ -9,9 +9,6 @@ def heapsort_a(iterable):
     heapq.heapify(iterable)
     return [heapq.heappop(iterable) for i in range(len(iterable))]
 
-# def heapsort_b(iterable):
-
-
 class maxHeap():
     def __init__(self, heap=None):
         self.heap = [] if heap is None else heap
@@ -24,8 +21,49 @@ class maxHeap():
         return self.heap
 
     def bubble_up(self, child_index):
-        parent_index = child_index//2
-        while self.heap[child_index] > self.heap[parent_index]:
-            self.heap[child_index], self.heap[parent_index] = self.heap[parent_index], self.heap[child_index]
-            child_index = parent_index
-            parent_index = child_index//2
+        while child_index > 0:
+            parent_index = (child_index-1)//2
+            if self.heap[child_index] > self.heap[parent_index]:
+                self.heap[child_index], self.heap[parent_index] = self.heap[parent_index], self.heap[child_index]
+                child_index = parent_index
+            else:
+                return
+
+    def bubble_down(self, parent_index):
+        while parent_index < len(self.heap) - 1:
+            max_child_index = self.get_max_child_index(parent_index)
+            if not max_child_index:
+                return
+            if self.heap[parent_index] < self.heap[max_child_index]:
+                self.heap[parent_index], self.heap[max_child_index] = self.heap[max_child_index], self.heap[parent_index]
+                parent_index = max_child_index
+            else:
+                return
+
+    def get_max_child_index(self, parent_index):
+        lchild_index = parent_index * 2 + 1
+        rchild_index = (parent_index + 1) * 2
+        if lchild_index > len(self.heap) - 1:
+            return None
+        elif rchild_index > len(self.heap) - 1:
+            return lchild_index
+        else:
+            return lchild_index if self.heap[lchild_index] >= self.heap[rchild_index] else rchild_index
+
+    def heapify(self, parent_index=False):
+        if parent_index:
+            max_child_index = self.get_max_child_index(parent_index)
+            if max_child_index:
+                self.bubble_up(self.get_max_child_index(parent_index))
+        else:
+            for i in range(len(self.heap)-1,0,-1):
+                self.bubble_up(i)
+                self.heapify(i)
+
+    def peek(self):
+        return self.heap[0]
+
+    def pop(self):
+        self.heap[0] = self.heap[len(self.heap) - 1]
+        self.heap.pop()
+        self.bubble_down(0)
