878a4499fdb24027af5b0930c7e841f24780bad0
Chris <christian.kneller@gmail.com>
2020-05-04 21:26:07 +0100

    Fixed quick and heap

--- sorting/heapsort.py
+++ sorting/heapsort.py
@@ -1,5 +1,6 @@
 import heapq
 
+
 def heapsort_a(iterable):
     '''
     Return a new list containing all items from the iterable in ascending order.
@@ -7,7 +8,22 @@ def heapsort_a(iterable):
     This utilises Python's built in heap.
     '''
     heapq.heapify(iterable)
-    return [heapq.heappop(iterable) for i in range(len(iterable))]
+    return [heapq.heappop(iterable) for _ in range(len(iterable))]
+
+
+# Oh no... why did I build a maxHeap class instead of a minHeap class? 
+# Now I have to reverse the final list or build a minHeap class from scratch!
+# TODO: build a minHeap class and save reversing the list at the end
+def heapsort_b(iterable):
+    '''
+    Return a new list containing all items from the iterable in ascending order.
+
+    This utilises a custom maxHeap class.
+    '''
+    myheap = maxHeap(iterable)
+    myheap.heapify()
+    return [myheap.pop() for _ in range(len(iterable))][::-1]
+
 
 class maxHeap():
     def __init__(self, heap=None):
@@ -51,19 +67,47 @@ class maxHeap():
             return lchild_index if self.heap[lchild_index] >= self.heap[rchild_index] else rchild_index
 
     def heapify(self, parent_index=False):
-        if parent_index:
-            max_child_index = self.get_max_child_index(parent_index)
-            if max_child_index:
-                self.bubble_up(self.get_max_child_index(parent_index))
-        else:
+        if parent_index == False:
             for i in range(len(self.heap)-1,0,-1):
-                self.bubble_up(i)
-                self.heapify(i)
+                parent_index = (i-1)//2
+                max_child_index = self.get_max_child_index(parent_index)
+                if self.heap[max_child_index] > self.heap[parent_index]:
+                    self.heap[max_child_index], self.heap[parent_index] = self.heap[parent_index], self.heap[max_child_index]
+                self.heapify(max_child_index)
+        else:
+            max_child_index = self.get_max_child_index(parent_index)
+            if not max_child_index:
+                return
+            if self.heap[max_child_index] > self.heap[parent_index]:
+                self.heap[max_child_index], self.heap[parent_index] = self.heap[parent_index], self.heap[max_child_index]
+            self.heapify(max_child_index)
 
     def peek(self):
         return self.heap[0]
 
     def pop(self):
+        highest = self.heap[0]
         self.heap[0] = self.heap[len(self.heap) - 1]
         self.heap.pop()
         self.bubble_down(0)
+        return highest
+
+def is_max_heap(heap: list, i: int, n: int) -> bool:
+    '''
+    Return a boolean value stating if the input list is a max heap.
+
+        Keyword arguments:
+            heap -- your input heap
+            i -- the index of the first value in the heap (0)
+            n -- the index of the last value in the heap (len(heap) - 1)
+    '''
+    if i > int((n-2)/2):
+        return True
+
+    if(heap[i] >= heap[2*i+1] and
+       heap[i] >= heap[2*i+2] and
+       is_max_heap(heap, 2*i+1,n) and
+       is_max_heap(heap, 2*i+2,n)):
+        return True
+
+    return False
--- sorting/quicksort.py
+++ sorting/quicksort.py
@@ -4,7 +4,7 @@ def quicksort(iterable):
     '''
     Return a new list containing all items from the iterable in ascending order.
 
-    This version takes the pivot to be the n//2th item in an iterable of length n+1.
+    This version takes the pivot to be the last item in an iterable.
     '''
     low = 0
     high = len(iterable)-1
@@ -14,49 +14,36 @@ def quicksort(iterable):
 
     return quicksorter(iterable,low,high)
 
-def quicksortrand(iterable):
-    '''
-    Return a new list containing all items from the iterable in ascending order.
-
-    This version takes the pivot to be a random item in the iterable.
-    '''
-    low = 0
-    high = len(iterable)-1
-
-    if high <= 0:
-        return iterable
-
-    return quicksorter(iterable,low,high,ptype='rand')
-
-
-def quicksorter(iterable, low, high, ptype='mid'):
+def quicksorter(iterable, low, high, ptype='hi'):
     if low >= high:
         return
 
     pivot = pivoter(iterable, low, high, ptype)
 
-    quicksorter(iterable, low, pivot)
+    quicksorter(iterable, low, pivot-1)
     
     quicksorter(iterable, pivot+1, high)
     
     return iterable
 
-def pivoter(iterable, low, high, ptype='mid'):
+def pivoter(iterable, low, high, ptype='hi'):
     
-    if ptype == 'mid':
-        pivot = low + (high-low)//2
-    elif ptype == 'rand':
-        pivot = random.randint(low, high)
+    if ptype == 'hi':
+        pivot = high
+
+    pivot_number = iterable[pivot]
 
     i = low - 1
 
-    for j in range(low, high+1):
-        if j == pivot:
-            pass
-        elif iterable[j] <= iterable[pivot]:
+    for j in range(low, high):
+        if iterable[j] <= pivot_number:
             i += 1
             iterable[i], iterable[j] = iterable[j], iterable[i]
         else:
             pass
+    
+    i += 1
 
-    return pivot
+    iterable[i], iterable[high] = iterable[high], iterable[i]
+
+    return i
